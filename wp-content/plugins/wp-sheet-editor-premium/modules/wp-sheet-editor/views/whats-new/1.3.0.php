783b326c33fce01d632e5a23231db69b1c7960e4
quentin-tiller <97976044+quentin-tiller@users.noreply.github.com>
2022-03-02 22:58:25 -0700

    restore files

--- wp-content/plugins/wp-sheet-editor-premium/modules/wp-sheet-editor/views/whats-new/1.3.0.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$items = array(
-	'fix_left_column' => __('<p>New feature: Now the first column of the spreadsheet is always visible while scrolling to the right.</p>', VGSE()->textname),
-	'minor' => __('<p>Other minor changes and fixes.</p>', VGSE()->textname),
-);
